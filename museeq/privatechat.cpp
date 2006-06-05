a12e15c27449712673b5fa0e307455a93dd6b9a6
Daelstorm <daelstorm@gmail.com>
2006-06-05 17:24:34 +0000

    Mucous: BIG Internal Variable rewrite, some fixes

--- museeq/privatechat.cpp
+++ museeq/privatechat.cpp
@@ -48,7 +48,7 @@ void PrivateChat::append(uint dir, uint ts, const QString& _u, const QString& _l
 }
 
 void PrivateChat::slotSend(const QString& line) {
-	if (!line.isEmpty()) {
+	if (! line.isEmpty()) {
 		museeq->sayPrivate(user(), line);
 		mChatPanel->append(QString::null, line);
 	}
