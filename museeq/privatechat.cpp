ca29df7da3d702c36c521f688c69cdef4f14d963
Daelstorm <daelstorm@gmail.com>
2006-07-13 23:28:48 +0000

    mikelabo's new silk icons, add private /commands, make museeq themes to be dynamically found by checking directory contents.

--- museeq/privatechat.cpp
+++ museeq/privatechat.cpp
@@ -25,7 +25,9 @@
 #include "codeccombo.h"
 #include "chatpanel.h"
 #include "museeq.h"
+#include "mainwin.h"
 #include "aclineedit.h"
+
 PrivateChat::PrivateChat(const QString& _t, QWidget* _p, const char* _n)
             : UserWidget(_t, _p, _n) {
 	
@@ -75,9 +77,95 @@ void PrivateChat::append(uint dir, uint ts, const QString& _u, const QString& _l
 	}
 }
 
-void PrivateChat::slotSend(const QString& line) {
+void PrivateChat::slotSend(const QString& line_) {
+	QString line = line_;
 	if (! line.isEmpty()) {
+		
+		if(line.startsWith("//")) {
+			line = line.mid(1);
+		} else if(line[0] == '/') {
+			QStringList s;
+			QString cmd("");
+			if(line.length() > 1)
+			{
+				s = QStringList::split(' ', line.mid(1));
+				cmd = s[0].lower();
+				s.pop_front();
+			}
+			
+			if(cmd == "me") {
+				museeq->sayPrivate(user(), line);
+				mChatPanel->append(QString::null, line);
+			} else if(cmd == "ip" ) {
+				if (s.empty())
+					museeq->mainwin()->showIPDialog(user());
+				else
+					museeq->mainwin()->showIPDialog(s.join(" "));
+			} else if((cmd == "c" || cmd == "chat")  &&  s.empty())
+				museeq->mainwin()->changeCMode();
+			else if(cmd == "pm" || cmd == "private") {
+				if(! s.empty())
+					museeq->mainwin()->showPrivateChat(s.join(" "));
+				else
+					museeq->mainwin()->changePMode();
+			} else if(cmd == "transfers" || cmd == "transfer")
+				museeq->mainwin()->changeTMode();
+			else if(cmd == "s" || cmd == "search") {
+				if(! s.empty())
+					museeq->mainwin()->startSearch(s.join(" "));
+				else
+					museeq->mainwin()->changeSMode();
+			} else if(cmd == "u" || cmd == "userinfo") {
+				if(! s.empty())
+					museeq->mainwin()->showUserInfo(s.join(" "));
+				else
+					museeq->mainwin()->showUserInfo(user());
+			} else if(cmd == "b" || cmd == "browse") {
+				if(! s.empty())
+					museeq->mainwin()->showBrowser(s.join(" "));
+				else
+					museeq->mainwin()->showBrowser(user());
+			} else if(cmd == "commands")
+				museeq->mainwin()->displayCommandsDialog();
+			else if(cmd == "about")
+				museeq->mainwin()->displayAboutDialog();
+			else if(cmd == "help")
+				museeq->mainwin()->displayHelpDialog();
+			else if(cmd == "ban" && s.empty())
+				museeq->addBanned(user(), "");
+			else if(cmd == "unban" && s.empty())
+				museeq->removeBanned(user());
+			else if(cmd == "ignore" &&  s.empty())
+				museeq->addIgnored(user(), "");
+			else if(cmd == "unignore" && s.empty())
+				museeq->removeIgnored(user());
+			else if(cmd == "buddy" && s.empty())
+				museeq->addBuddy(user(), "");
+			else if(cmd == "unbuddy" && s.empty())
+				museeq->removeBuddy(user());
+			else if(cmd == "trust" &&  s.empty())
+				museeq->addTrusted(user(), "");
+			else if(cmd == "distrust" &&  s.empty())
+				museeq->removeTrusted(user());
+			else if(cmd == "slap" && s.empty()) {
+				museeq->sayPrivate(user(), "/me slaps "+user()+" around with a large trout");
+				mChatPanel->append(QString::null, "/me slaps "+user()+" around with a large trout");
+			} else if((cmd == "j" || cmd == "join") && ! s.empty())
+				museeq->joinRoom(s.join(" "));
+			else if(cmd == "settings")
+				museeq->mainwin()->changeSettings();
+			else if(cmd == "log")
+				museeq->mainwin()->toggleLog();
+			else if(cmd == "ticker" || cmd == "tickers" || cmd == "t") 
+				museeq->mainwin()->toggleTickers();
+			else if(cmd == "colors" || cmd == "fonts" || cmd == "c" || cmd == "f") 
+				museeq->mainwin()->changeColors();
+			else
+				mChatPanel->entry()->setText(line);
+			return;
+			}	
 		museeq->sayPrivate(user(), line);
 		mChatPanel->append(QString::null, line);
 	}
 }
+
